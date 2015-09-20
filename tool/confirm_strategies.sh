60befcf8ba17414c3ee7b423311430bf3ac26c81
takahirox <hogehoge@gachapin.jp>
2015-09-20 00:15:32 -0700

    add tool/confirm_strategies.sh

---
+++ tool/confirm_strategies.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# = confirm_strategies.sh
+#
+# Author::    takahiro
+# Copyright:: takahiro
+# License::   GPL
+#
+
+# == Version
+#
+# * 2015-09-19 1.00 (takahiro) 
+#
+
+# == Summary
+# 
+# This file is for
+#
+
+# == Interface
+#
+# * input
+# **
+# * output
+# **
+#
+
+# == Example
+#
+
+# == Description
+#
+# This file is for
+#
+
+
+# function usage { 
+#   echo "usage : $0 <HOGE>"
+#   echo "ex ) $0 HOGE"
+#   exit
+# }
+#
+
+# while getopts "abd:h" options
+# do
+#   case $options in
+#     a | b ) ;;
+#     d     ) OPTDATA=${OPTARG} ;;
+#     h | * ) usage ;;
+#   esac
+# done
+# shift $(($OPTIND - 1))
+#
+
+
+# if [ $# -ne 1 ]; then
+#   usage
+# fi
+#
+# HOGE=$1
+#
+
+for file in $(ls ./strategies/run/*.sql)
+do
+  echo [log] $file
+  ./tool/run_strategy.sh $file
+done
+
