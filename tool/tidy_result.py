aac02e20e80e34a9437472301f3749e556f88f8a
takahirox <hogehoge@gachapin.jp>
2017-11-15 00:10:29 -0800

    Add percentage format to tidy_result.py git status

--- tool/tidy_result.py
+++ tool/tidy_result.py
@@ -42,11 +42,21 @@ else:
   exit()
 
 
-keys = ['sum', 'pf', 'p_w_fee',
-        'max_drawdown', 'max_s_wins', 'max_s_loses',
-        'average', 'deviation', 'p_average', 'p_deviation',
-        'c', 'p', 'n', 'max', 'min',
-        'pc', 'nc', 'p_pc', 'n_pc', 'pp', 'np']
+keys = ['sum', 'p_sum',
+        'pf', 'p_pf',
+        'p_w_fee', 'c',
+        'pc', 'nc',
+        'pp', 'np',
+        'max_dd', 'p_max_dd',
+        'max_s_wins', 'max_s_loses',
+        'average', 'p_average',
+        'deviation', 'p_deviation',
+        'p', 'p_p',
+        'n', 'p_n',
+        'max', 'p_max',
+        'min', 'p_min',
+        'p_pc', 'p_p_pc',
+        'n_pc', 'p_n_pc']
 
 
 def init_p():
@@ -59,14 +69,20 @@ def init_p():
   p['nc'] = 0
   p['max'] = 0.0
   p['min'] = 0.0
-  p['max_drawdown'] = 0.0
+  p['max_dd'] = 0.0
+  p['p_sum'] = 0.0
+  p['p_p'] = 0.0
+  p['p_n'] = 0.0
+  p['p_max'] = 0.0
+  p['p_min'] = 0.0
+  p['p_max_dd'] = 0.0
   p['max_s_wins'] = 0
   p['max_s_loses'] = 0
   # for work
   p['max_sum'] = 0.0
+  p['p_max_sum'] = 0.0
   p['suc_wins'] = 0
   p['suc_loses'] = 0
-  p['p_sum'] = 0.0
   p['values'] = []
   p['p_values'] = []
   return p
@@ -80,11 +96,13 @@ def add_value(p, value, p_value):
   p['p_values'].append(p_value)
   if value > 0.0:
     p['p'] += value
+    p['p_p'] += p_value
     p['pc'] += 1
     p['suc_wins'] += 1
     p['suc_loses'] = 0
-  if value < 0.0:
+  if value <= 0.0:
     p['n'] += value
+    p['p_n'] += p_value
     p['nc'] += 1
     p['suc_loses'] += 1
     p['suc_wins'] = 0
@@ -92,54 +110,100 @@ def add_value(p, value, p_value):
     p['max'] = value
   if value < p['min']:
     p['min'] = value
+  if p_value > p['p_max']:
+    p['p_max'] = p_value
+  if p_value < p['p_min']:
+    p['p_min'] = p_value
   if p['sum'] > p['max_sum']:
     p['max_sum'] = p['sum']
-  if p['sum'] - p['max_sum'] < p['max_drawdown']:
-    p['max_drawdown'] = p['sum'] - p['max_sum']
+  if p['sum'] - p['max_sum'] < p['max_dd']:
+    p['max_dd'] = p['sum'] - p['max_sum']
+  if p['p_sum'] > p['p_max_sum']:
+    p['p_max_sum'] = p['p_sum']
+  if p['p_sum'] - p['p_max_sum'] < p['p_max_dd']:
+    p['p_max_dd'] = p['p_sum'] - p['p_max_sum']
   if p['suc_wins'] > p['max_s_wins']:
     p['max_s_wins'] = p['suc_wins']
   if p['suc_loses'] > p['max_s_loses']:
     p['max_s_loses'] = p['suc_loses']
 
 
+def get_unit():
+  if options.market == 'usdjpy_daily':
+    return 25000
+  return 100
+
+
+def get_fee():
+  if options.market == 'usdjpy_daily':
+    return 400
+  return 100
+
+
 def calc_value(p):
-  p['p_w_fee'] = p['sum'] * 100 - p['c'] * 100
+  p['p_w_fee'] = p['sum'] * get_unit() - p['c'] * get_fee()
 
   if p['n'] == 0.0:
     p['pf'] = float('inf')
   else:
     p['pf'] = p['p'] / -p['n']
 
+  if p['p_n'] == 0.0:
+    p['p_pf'] = float('inf')
+  else:
+    p['p_pf'] = p['p_p'] / -p['p_n']
+
   if p['pc'] == 0:
     p['p_pc'] = float('inf')
+    p['p_p_pc'] = float('inf')
   else:
     p['p_pc'] = p['p'] / p['pc']
+    p['p_p_pc'] = p['p_p'] / p['pc']
 
   if p['nc'] == 0:
     p['n_pc'] = float('inf')
+    p['p_n_pc'] = float('inf')
   else:
     p['n_pc'] = p['n'] / p['nc']
+    p['p_n_pc'] = p['p_n'] / p['nc']
 
-  p['pp'] = float(p['pc']) / float(p['c']) * 100.0
-  p['np'] = float(p['nc']) / float(p['c']) * 100.0
+  if p['c'] == 0:
+    p['pp'] = float('inf')
+    p['np'] = float('inf')
+    p['average'] = 0
+    p['deviation'] = 0.0
+    p['p_average'] = 0.0
+    p['p_deviation'] = 0.0
+  else:
+    p['pp'] = float(p['pc']) / float(p['c']) * 100.0
+    p['np'] = float(p['nc']) / float(p['c']) * 100.0
 
-  ave = float(p['sum']) / float(p['c'])
-  s = 0.0
+    ave = float(p['sum']) / float(p['c'])
+    s = 0.0
 
-  for v in p['values']:
-    s += pow(v - ave, 2)
+    for v in p['values']:
+      s += pow(v - ave, 2)
 
-  p['average'] = ave
-  p['deviation'] = math.sqrt(s / p['c'])
+    p['average'] = ave
+    p['deviation'] = math.sqrt(s / p['c'])
 
-  ave = float(p['p_sum']) / float(p['c'])
-  s = 0.0
+    ave = float(p['p_sum']) / float(p['c'])
+    s = 0.0
 
-  for v in p['p_values']:
-    s += pow(v - ave, 2)
+    for v in p['p_values']:
+      s += pow(v - ave, 2)
 
-  p['p_average'] = ave * 100.0
-  p['p_deviation'] = math.sqrt(s / p['c']) * 100.0
+    p['p_average'] = ave * 100.0
+    p['p_deviation'] = math.sqrt(s / p['c']) * 100.0
+
+  p['p_sum'] *= 100.0
+  p['p_max_dd'] *= 100.0
+  p['p_p'] *= 100.0
+  p['p_n'] *= 100.0
+  p['p_max'] *= 100.0
+  p['p_min'] *= 100.0
+  p['p_p_pc'] *= 100.0
+  p['p_n_pc'] *= 100.0
 
 def display_header():
   for k in keys:
@@ -154,8 +218,12 @@ def display_p(p):
 
 
 def display_p2(p):
+  i = 0
   for k in keys:
-    print '%12s : %12.2f' % (k, p[k])
+    print '%12s : %12.2f' % (k, p[k]),
+    if i % 2 == 1:
+      print
+    i += 1
 
 
 total_p = init_p()
